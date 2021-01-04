d5980554a1cbe19ef897842620fb929ef7c72895
Nicholas Werner <ncwerner@gmail.com>
2021-01-04 10:43:19 -0700

    created init function

--- employeeTracker.js
+++ employeeTracker.js
@@ -1,5 +1,7 @@
-var mysql = require("mysql");
-var inquirer = require("inquirer");
+const mysql = require("mysql");
+const inquirer = require("inquirer");
+const cTable = require('console.table');
+
 
 // create the connection information for the sql database
 var connection = mysql.createConnection({
@@ -19,5 +21,25 @@ var connection = mysql.createConnection({
 connection.connect(function(err) {
     if (err) throw err;
     console.log("connected as id " + connection.threadId + "\n");
-    
+    start()
   });
+
+  function start() {
+    inquirer
+    .prompt({
+      type: "list",
+      name: "task",
+      message: "Would you like to do?",
+      choices: [
+        "View Employees",
+        "View Employees by Department",
+        // "View Employees by Manager",
+        "Add Employee",
+        "Remove Employees",
+        "Update Employee Role",
+        "Add Role",
+        // "Remove Role",
+        // "Update Employee Manager",
+        "End"]
+    })
+  }
