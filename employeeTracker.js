43ace73bc3e57d3b6e1918e57ee60f4db80b8fea
Nicholas Werner <ncwerner@gmail.com>
2021-01-04 10:19:56 -0700

    installed MySql, Inquirer and consol.table + added gitignore

--- employeeTracker.js
+++ employeeTracker.js
@@ -5,8 +5,8 @@ var inquirer = require("inquirer");
 var connection = mysql.createConnection({
   host: "localhost",
 
-  // Your port; if not 8080
-  port: 8080,
+  // Your port; if not 3306
+  port: 3306,
 
   // Your username
   user: "root",
@@ -15,3 +15,9 @@ var connection = mysql.createConnection({
   password: "MySQL85!",
   database: "employeeTracker_DB"
 });
+
+connection.connect(function(err) {
+    if (err) throw err;
+    console.log("connected as id " + connection.threadId + "\n");
+    
+  });
