6f28859d05817f2b5011d8db60b8998a22313404
Nicholas Werner <ncwerner@gmail.com>
2021-01-06 10:18:58 -0700

    created prompts for the user

--- employeeTracker.js
+++ employeeTracker.js
@@ -17,29 +17,54 @@ var connection = mysql.createConnection({
   password: "MySQL85!",
   database: "employeeTracker_DB"
 });
-
+// connect to the mysql server and sql database
 connection.connect(function(err) {
     if (err) throw err;
     console.log("connected as id " + connection.threadId + "\n");
     start()
   });
-
+// function which prompts the user for what action they should take
   function start() {
     inquirer
     .prompt({
       type: "list",
-      name: "task",
+      name: "start",
       message: "Would you like to do?",
       choices: [
         "View Employees",
-        "View Employees by Department",
-        // "View Employees by Manager",
         "Add Employee",
-        "Remove Employees",
-        "Update Employee Role",
+        "View Department",
+        "Add Department",
+        "View Role",
         "Add Role",
-        // "Remove Role",
-        // "Update Employee Manager",
-        "End"]
+        "Update Employee Role",
+        "Exit"
+    ]
     })
+    .then(function(answer) {
+        if (answer.start === "View Employees") {
+            viewEE();
+        }
+        else if (answer.start === "Add Employee") {
+            addEE();
+        }
+        else if (answer.start === "View Department") {
+            viewDepartment();
+        }
+        else if (answer.start === "Add Department") {
+            addDepartment();
+        }
+        else if (answer.start === "View Role") {
+            viewRole();
+        }
+        else if (answer.start === "Add Role") {
+            addRole();
+        }
+        else if (answer.start === "Update Employee Role") {
+            updateEErole();
+        }
+        else{
+            connection.end();
+        }
+    });
   }
